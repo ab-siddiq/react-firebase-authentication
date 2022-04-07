0b503dd704dd2854eea82b72a50059f236e5ed76
siddiq <siddiq.aiub@gmail.com>
2022-04-07 21:40:19 +0600

    created regiter using email

--- README.md
+++ README.md
@@ -3,9 +3,12 @@
 ## What is used in this project?
 
 - `Firebase` for user login authentication
+    - `Gmail Sing In`
+    - `Github Sign In`
+    - `Email Sign In`
 - `React.js` for building this project
 - `tailwind css` for ui design
-- `Router` for navigating pages
+- `React Router` for navigating pages
 
 
 
--- src/components/Register/Register.js
+++ src/components/Register/Register.js
@@ -1,16 +1,29 @@
-import React from 'react';
-
+import { createUserWithEmailAndPassword, getAuth } from 'firebase/auth';
+import React, { useState } from 'react';
+const auth = getAuth();
 const Register = () => {
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('');
+
     const handleFormSubmit = event => {
+        console.log(email,'=>',password)
+        createUserWithEmailAndPassword(auth, email, password)
+            .then(result => {
+                const user = result.user;
+                console.log(user)
+            })
+            .catch(error => {
+                console.error(error);
+            });
         event.preventDefault();
-        console.log('submitted')
+      
 
     }
     const handleEmailBlur = event => {
-        console.log(event.target.value)
+        setEmail(event.target.value)
     }
     const handlePasswordBlur = event => {
-        console.log(event.target.value)
+        setPassword(event.target.value)
     }
     
     return (
@@ -26,7 +39,7 @@ const Register = () => {
                         <input onBlur={handlePasswordBlur} className='mt-1 px-3 py-2 bg-white border shadow-sm border-slate-300 placeholder-slate-400 focus:outline-none focus:border-sky-500 focus:ring-sky-500 block w-full rounded-md sm:text-sm focus:ring-1' type="password" name="" id="" placeholder='set a password' />
                     </div>
                     <div className="mt-6 ">
-                        <input className='bg-green-700 text-green-50 px-3 py-2 w-full rounded cursor-pointer' type="submit" value="Register" />
+                        <input className='bg-green-700 text-green-50 px-3 py-2 w-full rounded cursor-pointer hover:scale-105 duration-200' type="submit" value="Register" />
                     </div>
 
                 </div>
